4795f803a116650eccb23fe3fcbb792ed06580a3
Stephen Pearce <hello@stevepdp.dev>
2023-01-31 23:35:49 +0000

    feat(enemy): utilise NavMeshComponents to allow enemies to chase the player
    This change sets up NavMesh on the enemy prefab and configures for basic
    chasing.
    
    Adjustments were also made to the colliders for each of the player and
    enemies to ensure that the player can be pushed off surfaces and into
    deadzones.

--- Assets/Scripts/Enemy.cs
+++ Assets/Scripts/Enemy.cs
@@ -2,25 +2,55 @@ using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.AI;
 
 public class Enemy : MonoBehaviour
 {
     public static event Action OnEnemyDead;
-
+    
     [SerializeField] int hp = 1;
+    [SerializeField] private NavMeshAgent agent;
+    [SerializeField] private Transform player;
+    [SerializeField] private Vector3 startPos;
 
-    private void OnCollisionEnter(Collision other)
+    void Awake()
     {
-        // TODO: To be replaced with player's projectile
+        SetupEnemy();
+        GetPlayerTransform();
+    }
+
+    void Update()
+    {
+        ChasePlayer();
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
         if (other.gameObject.CompareTag("Player"))
         {
-            DeductHP();
+            //DeductHP();
         }
     }
 
-    public void DeductHP()
+    void ChasePlayer()
+    {
+        if (player != null)
+        {
+            agent.destination = player.transform.position;
+        }
+    }
+
+    void DeductHP()
     {
         hp--;
         if (hp <= 0) Destroy(gameObject);
+    }
+
+    void GetPlayerTransform() => player = GameObject.FindObjectOfType<Player>().GetComponent<Transform>();
+
+    void SetupEnemy()
+    {
+        agent = GetComponent<NavMeshAgent>();
+        startPos = transform.position;
     }
 }
