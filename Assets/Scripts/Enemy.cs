19b929b493187f7cd1070c882bc1692e641dd0f0
Stephen Pearce <hello@stevepdp.dev>
2023-02-02 18:32:15 +0000

    feat(enemy): add "return to base" functionality
    This change introduces the ability for the enemy to give up
    chasing the player and return to it's base/start position.
    
    Additonally it gives colour to the enemy depending on aggro state.

--- Assets/Scripts/Enemy.cs
+++ Assets/Scripts/Enemy.cs
@@ -7,14 +7,24 @@ using UnityEngine.AI;
 public class Enemy : MonoBehaviour
 {
     public static event Action OnEnemyDead;
-    
-    [SerializeField] int hp = 1;
+
+    [SerializeField] private bool hasChased;
+    [SerializeField] private bool isChasing;
+    [SerializeField] private bool isIdle;
+    [SerializeField] private int aggroDistance = 7;
+    [SerializeField] private int giveUpDistance = 14;
+    [SerializeField] private int hp = 1;
     [SerializeField] private NavMeshAgent agent;
     [SerializeField] private Transform player;
-    [SerializeField] private Vector3 startPos;
+    [SerializeField] private Vector3 basePos;
+
+    private Color startingColour;
+    private Renderer renderer;
 
     void Awake()
     {
+        renderer = GetComponent<Renderer>();
+
         SetupEnemy();
         GetPlayerTransform();
     }
@@ -36,7 +46,36 @@ public class Enemy : MonoBehaviour
     {
         if (player != null)
         {
-            agent.destination = player.transform.position;
+            float distanceFromPlayer = Vector3.Distance(transform.position, player.transform.position);
+            float distanceFromStart = Vector3.Distance(transform.position, basePos);
+            float playerDistanceFromStart = Vector3.Distance(player.transform.position, basePos);
+
+            if (!hasChased && !isChasing && distanceFromPlayer <= aggroDistance)
+            {
+                // Start chasing
+                hasChased = true;
+                isChasing = true;
+                renderer.material.color = Color.red;
+                agent.destination = player.transform.position;
+            }
+            else if (hasChased && isChasing && distanceFromStart < giveUpDistance)
+            {
+                // Continue chasing
+                agent.destination = player.transform.position;
+            }
+            else if (hasChased && isChasing && distanceFromStart >= giveUpDistance)
+            {
+                // Give up
+                isChasing = false;
+                agent.destination = basePos;
+                renderer.material.color = Color.magenta;
+            }
+            else if (hasChased && !isChasing && agent.velocity == Vector3.zero)
+            {
+                // Back on guard
+                renderer.material.color = startingColour;
+                hasChased = false;
+            }
         }
     }
 
@@ -51,6 +90,8 @@ public class Enemy : MonoBehaviour
     void SetupEnemy()
     {
         agent = GetComponent<NavMeshAgent>();
-        startPos = transform.position;
+        basePos = transform.position;
+        isIdle = true;
+        startingColour = renderer.material.color;
     }
 }
