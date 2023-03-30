5a86056a0ff1aead075cba42c07dd6eb7ad8d5ca
Stephen Pearce <hello@stevepdp.dev>
2023-03-31 00:47:57 +0100

    refactor(hud): redundant HUDInput class removed

--- Assets/Scripts/HUDCamera.cs
+++ Assets/Scripts/HUDCamera.cs
@@ -1,25 +1,38 @@
+using System;
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class HUDCamera : MonoBehaviour
 {
+    public static event Action OnToggleHUD;
+
     Camera canvasCam;
+    InputAction toggleHUD;
+    PlayerControls playerControls;
 
     void Awake()
     {
         canvasCam = GetComponent<Camera>();
+        playerControls = new PlayerControls();
     }
 
     void OnEnable()
     {
-        HUDInput.OnToggleHUD += ToggleHUD;
+        if (playerControls != null)
+        {
+            toggleHUD = playerControls.Player.ToggleHUD;
+            toggleHUD.Enable();
+            toggleHUD.performed += ToggleHUD;
+        }
     }
 
     void OnDisable()
     {
-        HUDInput.OnToggleHUD -= ToggleHUD;
+        toggleHUD.performed -= ToggleHUD;
+        toggleHUD.Disable();
     }
 
-    void ToggleHUD()
+    void ToggleHUD(InputAction.CallbackContext context)
     {
         if (canvasCam != null)
             canvasCam.enabled = !canvasCam.enabled;
